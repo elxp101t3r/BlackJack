8482d2312233bf33784db4acbe7e139759c55ef8
elxp101t3r <eliotx10101@outlook.com>
2023-10-25 20:34:12 +0300

    Form the rules

--- main.py
+++ main.py
@@ -22,3 +22,24 @@ def start_game():
         if sum(user_cards) is None:
             return 0
         return sum(user_cards)
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+    game_over = False
+    computer_score = 0
+    user_score = 0
+    while not game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f'Your cards: {user_cards}\nCurrent Score: {user_score}')
+        print(f'Computer first card: {computer_cards[0]}')
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            game_over = True
+        else:
+            another_card = input('Type "y" to get another card or "n" to stop: ')
+            if another_card == 'y':
+                user_cards.append(deal_card())
+            else:
+                game_over = True
+        
+            
