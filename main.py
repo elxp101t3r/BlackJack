1bffa3cac79e01cd13f31e27af6755df0e4c82c3
elxp101t3r <eliotx10101@outlook.com>
2023-10-25 20:28:16 +0300

    User Score Calc

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ def start_game():
         else:
             print('There are no more cards..')
             return None
+    user_cards = []
+    computer_cards = []
+    def calculate_score(user_cards):
+        if sum(user_cards) == 21 and len(user_cards) == 2:
+            return 0
+        if 11 in user_cards and sum(user_cards) > 21:
+            user_cards.remove(11)
+            user_cards.append(1)
+        if sum(user_cards) is None:
+            return 0
+        return sum(user_cards)
