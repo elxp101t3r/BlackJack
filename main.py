47a343ed01fc9d7148e5964a0079322fde64c978
elxp101t3r <eliotx10101@outlook.com>
2023-10-25 20:24:31 +0300

    The dealer function

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from art import logo
-print(logo)
+import random as r
+import replit as rep
+
+# print(logo)
+def start_game():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    def deal_card():
+        if cards:
+            random_card = r.choice(cards)
+            cards.remove(random_card)
+            return random_card
+        else:
+            print('There are no more cards..')
+            return None
